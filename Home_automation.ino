d9486beaf3743cb399c79481e7e00d8b430d4b27
rohitkverma1998 <45102123+rohitkverma1998@users.noreply.github.com>
2019-11-07 21:53:40 +0530

    code added

---
+++ Home_automation.ino
@@ -0,0 +1,238 @@
+
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+
+char auth[] = "fb390b1f6e7747d2b44e7cfb446cce7d";
+
+
+char ssid[] = "Razdaan";
+char pass[] = "bhardwaj";
+
+
+int pinin1;
+int pinin11;
+int pinin2;
+int pinin22;
+int pinin3;
+int pinin33;
+int pinin4;
+int pinin44;
+int pinin5;
+int pinin55;
+int pinout1 = 0;
+int pinout2 = 2;
+int pinout3 = 4;
+int pinout4 = 5;
+int pinout5 = 16;
+
+       
+void setup()
+{
+
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid , pass);
+
+  pinMode(pinout1,OUTPUT);
+  pinMode(pinout2,OUTPUT);
+  pinMode(pinout3,OUTPUT);
+  pinMode(pinout4,OUTPUT);
+  pinMode(pinout5,OUTPUT);
+
+  
+}
+
+
+
+
+   BLYNK_WRITE(V1) // V5 is the number of Virtual Pin  
+   {
+              pinin1 = param.asInt();
+   }
+   BLYNK_WRITE(V2) // V5 is the number of Virtual Pin  
+   {
+              pinin11 = param.asInt();
+   }
+   BLYNK_WRITE(V3) // V5 is the number of Virtual Pin  
+   {
+              pinin2 = param.asInt();
+   }
+   BLYNK_WRITE(V4) // V5 is the number of Virtual Pin  
+   {
+              pinin22 = param.asInt();
+   }
+   BLYNK_WRITE(V5) // V5 is the number of Virtual Pin  
+   {
+              pinin3 = param.asInt();
+   }
+   BLYNK_WRITE(V6) // V5 is the number of Virtual Pin  
+   {
+              pinin33 = param.asInt();
+   }
+   BLYNK_WRITE(V7) // V5 is the number of Virtual Pin  
+   {
+              pinin4 = param.asInt();
+   }
+   BLYNK_WRITE(V8) // V5 is the number of Virtual Pin  
+   {
+              pinin44 = param.asInt();
+   }
+   BLYNK_WRITE(V9) // V5 is the number of Virtual Pin  
+   {
+              pinin5 = param.asInt();
+   }
+   BLYNK_WRITE(V10) // V5 is the number of Virtual Pin  
+   {
+              pinin55 = param.asInt();
+   }
+
+
+
+
+
+void loop()
+{   
+        
+
+//1 11 1
+    if((pinin1) == HIGH)
+    {
+          if(WiFi.status() != WL_CONNECTED && (pinin11) == LOW)
+          {
+              digitalWrite(pinout1,LOW);
+          }
+          else if(WiFi.status() == WL_CONNECTED)
+          {
+              digitalWrite(pinout1,HIGH);
+          }
+    }
+  
+    else if((pinin1) == LOW)
+    {  
+          if((pinin11) == LOW)
+          {
+              digitalWrite(pinout1,LOW);  
+          }
+          else if((pinin11) == HIGH)
+          {
+              digitalWrite(pinout1,HIGH);  
+          }
+    }
+
+//2 22 2
+    if((pinin2) == HIGH)
+    {
+          if(WiFi.status() != WL_CONNECTED && (pinin22) == LOW)
+          {
+              digitalWrite(pinout2,LOW);
+          }
+          else if(WiFi.status() == WL_CONNECTED)
+          {
+              digitalWrite(pinout2,HIGH);
+          }
+    }
+  
+    else if((pinin2) == LOW)
+    {  
+          if((pinin22) == LOW)
+          {
+              digitalWrite(pinout2,LOW);  
+          }
+          else if((pinin22) == HIGH)
+          {
+              digitalWrite(pinout2,HIGH);  
+          }
+    }
+
+
+ //3 33 3
+    if((pinin3) == HIGH)
+    {
+          if(WiFi.status() != WL_CONNECTED && (pinin33) == LOW)
+          {
+              digitalWrite(pinout3,LOW);
+          }
+          else if(WiFi.status() == WL_CONNECTED)
+          {
+              digitalWrite(pinout3,HIGH);
+          }
+    }
+  
+    else if((pinin3) == LOW)
+    {  
+          if((pinin33) == LOW)
+          {
+              digitalWrite(pinout3,LOW);  
+          }
+          else if((pinin33) == HIGH)
+          {
+              digitalWrite(pinout3,HIGH);  
+          }
+    }
+
+
+
+ //4 44 4
+    if((pinin4) == HIGH)
+    {
+          if(WiFi.status() != WL_CONNECTED && (pinin44) == LOW)
+          {
+              digitalWrite(pinout4,LOW);
+          }
+          else if(WiFi.status() == WL_CONNECTED)
+          {
+              digitalWrite(pinout4,HIGH);
+          }
+    }
+  
+    else if((pinin4) == LOW)
+    {  
+          if((pinin44) == LOW)
+          {
+              digitalWrite(pinout4,LOW);  
+          }
+          else if((pinin44) == HIGH)
+          {
+              digitalWrite(pinout4,HIGH);  
+          }
+    }
+
+
+
+ //5 55 5
+    if((pinin5) == HIGH)
+    {
+          if(WiFi.status() != WL_CONNECTED && (pinin55) == LOW)
+          {
+              digitalWrite(pinout5,LOW);
+          }
+          else if(WiFi.status() == WL_CONNECTED)
+          {
+              digitalWrite(pinout5,HIGH);
+          }
+    }
+  
+    else if((pinin5) == LOW)
+    {  
+          if((pinin55) == LOW)
+          {
+              digitalWrite(pinout5,LOW);  
+          }
+          else if((pinin55) == HIGH)
+          {
+              digitalWrite(pinout5,HIGH);  
+          }
+    }
+
+
+
+    
+    Blynk.run();
+
+
+
+}
+
---
+++ servo_robo.ino
@@ -0,0 +1,159 @@
+
+#include <Servo.h>
+
+Servo myservo1;
+Servo myservo2;
+
+char t;
+int enA = 3;   
+int enB = 5; 
+
+void setup() {
+
+myservo1.attach(6);
+myservo2.attach(7);
+
+pinMode(enA, OUTPUT); 
+pinMode(enB, OUTPUT); 
+pinMode(9,OUTPUT);   //left motors forward
+pinMode(10,OUTPUT);   //left motors reverse
+pinMode(11,OUTPUT);   //right motors forward
+pinMode(12,OUTPUT);   //right motors reverse
+ 
+Serial.begin(9600);
+ 
+}
+ 
+void loop() {
+if(Serial.available()){
+  t = Serial.read();
+  Serial.println(t);
+}
+ 
+if(t == 'R'){            //move forward(all motors rotate in forward direction)
+  digitalWrite(9,HIGH);
+  digitalWrite(10,LOW);
+  digitalWrite(11,HIGH);
+  digitalWrite(12,LOW);
+}
+ 
+else if(t == 'L'){      //move reverse (all motors rotate in reverse direction)
+  digitalWrite(9,LOW);
+  digitalWrite(10,HIGH);
+  digitalWrite(11,LOW);
+  digitalWrite(12,HIGH);
+}
+ 
+else if(t == 'F'){      //turn right (left side motors rotate in forward direction, right side motors doesn't rotate)
+  digitalWrite(9,LOW);
+  digitalWrite(10,HIGH);
+  digitalWrite(11,HIGH);
+  digitalWrite(12,LOW);
+}
+ 
+else if(t == 'B'){      //turn left (right side motors rotate in forward direction, left side motors doesn't rotate)
+  digitalWrite(9,HIGH);
+  digitalWrite(10,LOW);
+  digitalWrite(11,LOW);
+  digitalWrite(12,HIGH);
+}
+ 
+else if(t == 'S'){      //STOP (all motors stop)
+  digitalWrite(9,LOW);
+  digitalWrite(10,LOW);
+  digitalWrite(11,LOW);
+  digitalWrite(12,LOW);
+}
+
+else if(t == '0')
+{
+  analogWrite(enA, 100);
+  analogWrite(enB, 100);
+}
+
+else if(t == '1')
+{
+  analogWrite(enA, 140);
+  analogWrite(enB, 140);
+}
+
+else if(t == '2')
+{
+  analogWrite(enA, 153);
+  analogWrite(enB, 153);
+}
+
+else if(t == '3')
+{
+  analogWrite(enA, 165);
+  analogWrite(enB, 165);
+}
+
+else if(t == '4')
+{
+  analogWrite(enA, 178);
+  analogWrite(enB, 178);
+}
+
+else if(t == '5')
+{
+  analogWrite(enA, 191);
+  analogWrite(enB, 191);
+}
+
+else if(t == '6')
+{
+  analogWrite(enA, 204);
+  analogWrite(enB, 204);
+}
+
+else if(t == '7')
+{
+  analogWrite(enA, 216);
+  analogWrite(enB, 216);
+}
+
+else if(t == '8')
+{
+  analogWrite(enA, 229);
+  analogWrite(enB, 229);
+}
+
+else if(t == '9')
+{
+  analogWrite(enA, 242);
+  analogWrite(enB, 242);
+}
+
+else if(t == 'q')
+{
+  analogWrite(enA, 255);
+  analogWrite(enB, 255);
+}
+
+else if(t == 'W')
+{
+  myservo1.write(0);
+}
+
+else if(t == 'w')
+{
+  myservo1.write(90);
+}
+
+
+else if(t == 'U')
+{
+  myservo1.write(-10);
+}
+
+else if(t == 'u')
+{
+  myservo1.write(0);
+}
+
+
+
+
+
+}
